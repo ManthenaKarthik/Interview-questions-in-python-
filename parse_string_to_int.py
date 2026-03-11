4b2c9467a2097f2dc8770e19cebc7784187c5ba1
ManthenaKarthik <manthenakarthik604@gmail.com>
2026-03-11 22:05:20 +0530

    Add files via upload

---
+++ parse_string_to_int.py
@@ -0,0 +1,9 @@
+number = int(input("enter an integer value :"))
+balance_str = number
+balance_int = int(balance_str)
+
+# print the type
+print(type(balance_int))
+
+# print the value
+print(balance_int)

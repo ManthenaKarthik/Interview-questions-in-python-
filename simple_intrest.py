d7ade6e159922e2127af90547fbf4dfcb2e33a44
ManthenaKarthik <manthenakarthik604@gmail.com>
2026-03-11 22:04:51 +0530

    Add files via upload

---
+++ simple_intrest.py
@@ -0,0 +1,10 @@
+p = int(input("enter p value :"))
+t = int(input("enter t value :"))
+r = int(input("enter r value :"))
+def fun(p, t, r):
+    return (p * t * r) / 100
+
+p, t, r 
+
+res = fun(p, t, r)
+print(res)
